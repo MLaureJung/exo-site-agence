f6df38ae01ed0f6bf6a04216e344418be2ee69e7
MLaureJung <mlaure.jung@gmail.com>
2019-12-29 12:55:18 +0100

    modifs slider + session grid actu masonry

--- webpack-oagency/app/js/app.js
+++ webpack-oagency/app/js/app.js
@@ -1,7 +1,20 @@
+var jQueryBridget = require('jquery-bridget');
+var Masonry = require('masonry-layout');
+jQueryBridget( 'masonry', Masonry, $ );
+
 var app = {
   init: function() {
     console.log('init');
+
+    $('.grid').masonry({
+      // options
+      itemSelector: '.grid-item',
+      columnWidth: 200,
+      gutter: 10
+      
+    })
   }
+
 };
 
 $(app.init);
